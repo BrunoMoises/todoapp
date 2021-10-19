876281c12b0830ea9c89e4aa3f32a8df720737e0
Bruno Moisés <bruno.moises@estudante.iftm.edu.br>
2021-10-18 22:43:27 -0300

    Inserindo validacao de switch e adicionando scroll view

--- app/src/main/java/br/edu/iftm/pdm/todoapp/MainActivity.kt
+++ app/src/main/java/br/edu/iftm/pdm/todoapp/MainActivity.kt
@@ -4,24 +4,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Switch
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private var etxtInteger: EditText? = null
+    private var etxtTarefa: EditText? = null
     private var txtHistory: TextView? = null
+    private var swtUrgent: Switch? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        this.etxtInteger = findViewById(R.id.etxtTarefa)
+        this.etxtTarefa = findViewById(R.id.etxtTarefa)
         this.txtHistory = findViewById(R.id.txtHistory)
+        this.swtUrgent = findViewById(R.id.swtUrgent)
     }
 
     fun onClickSend(v: View) {
-        val valueStr: String = this.etxtInteger?.text.toString()
+        val valueStr: String = this.etxtTarefa?.text.toString()
         if(valueStr.isNotEmpty()) {
-            this.txtHistory?.append(valueStr)
-            this.etxtInteger?.setText("")
+            if(this.swtUrgent?.isChecked == true) {
+                this.txtHistory?.append(getString(R.string.urgent, valueStr))
+            } else {
+                this.txtHistory?.append(getString(R.string.not_urgent, valueStr))
+            }
+            this.txtHistory?.append("\n")
+            this.etxtTarefa?.setText("")
         }
-        this.txtHistory?.append("\n")
     }
 }
