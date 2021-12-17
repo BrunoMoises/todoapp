2748b627b9b7b616286bd000eab4b27cf4a4c424
Bruno Moisés <bruno.moises@estudante.iftm.edu.br>
2021-12-16 21:38:58 -0300

    Fazendo a programação para inserir os cards e finalizando a funcionalidade do app

--- app/src/main/java/br/edu/iftm/pdm/todoapp/ChannelDAOSingleton.kt
+++
@@ -1,5 +0,0 @@
-package br.edu.iftm.pdm.todoapp
-
-object ChannelDAOSingleton {
-    private val
-}
--- app/src/main/java/br/edu/iftm/pdm/todoapp/MainActivity.kt
+++ app/src/main/java/br/edu/iftm/pdm/todoapp/MainActivity.kt
@@ -1,31 +1,39 @@
 package br.edu.iftm.pdm.todoapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.Switch
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private var etxtTarefa: EditText? = null
-    private var swtUrgent: Switch? = null
+    private lateinit var etxtTarefa: EditText
+    private lateinit var swtUrgent: Switch
+    private lateinit var lnvTaskList: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         this.etxtTarefa = findViewById(R.id.etxtTarefa)
         this.swtUrgent = findViewById(R.id.swtUrgent)
+        this.lnvTaskList = findViewById(R.id.lnvTaskList)
     }
 
     fun onClickSend(v: View) {
-        val valueStr: String = this.etxtTarefa?.text.toString()
-        if(valueStr.isNotEmpty()) {
-            if(this.swtUrgent?.isChecked == true) {
-                this.txtHistory?.append(getString(R.string.urgent, valueStr))
+        if(this.etxtTarefa.text.toString().isNotEmpty()) {
+            val itemView = View.inflate(this, R.layout.card_layout, null)
+            val txtBackground: TextView = itemView.findViewById(R.id.txtBackground)
+            val txtTask: TextView = itemView.findViewById(R.id.txtTask)
+            txtTask.text = this.etxtTarefa.text.toString()
+            if(this.swtUrgent.isChecked) {
+                txtBackground.setBackgroundColor(Color.RED)
             } else {
-                this.txtHistory?.append(getString(R.string.not_urgent, valueStr))
+                txtBackground.setBackgroundColor(Color.GREEN)
             }
-            this.etxtTarefa?.setText("")
+            this.lnvTaskList.addView(itemView)
+            this.etxtTarefa.setText("")
         }
     }
 }
